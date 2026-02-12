c064f7c7c2f81a9f6c2b7ab4a65e82bccc045d46
NecyNazy <105193755+NecyNazy@users.noreply.github.com>
2026-02-12 15:12:35 +0100

    User input and final logic

--- src/main/java/org/Necy/UserInput.java
+++ src/main/java/org/Necy/UserInput.java
@@ -6,5 +6,47 @@ import static org.Necy.InputCalculation.*;
 
 public record UserInput(Scanner scanner) {
 
+    public void addNumbers() {
+        System.out.println("Enter two numbers.");
+        System.out.println("Enter first number: " );
+        int x = enterNumber();
+        System.out.println("Enter second number: " );
+        int y = enterNumber();
+        int result = add(x, y);
+        System.out.println(result);
+    }
+
+    public void evenOrOddNumber() {
+        System.out.println("Enter a number:");
+        int a = enterNumber();
+        String result = evenOrOdd(a);
+        System.out.println(result);
+    }
+
+    public void printUptoN() {
+        System.out.println("Enter a number");
+        int n = enterNumber();
+        printN(n);
+    }
+
+    public void displayDay() {
+        System.out.println("Please enter a number between 1 and 7");
+        int n = enterNumber();
+        String result = displayDayOfWeek(n);
+        System.out.println(result);
+    }
+
+    public void exit() {
+        System.out.println("Thank you for using the app");
+    }
+
+    //HELPER: This handles invalid cases where user makes invalid input
+    private int enterNumber() {
+        while (!scanner.hasNextInt()) {
+            System.out.println("Invalid input! Please input a number");
+            scanner.next(); // this helps to clear invalid text so java doesn't get stuck while trying to process the invalid input
+        }
+        return scanner.nextInt();
+    }
 
 }
